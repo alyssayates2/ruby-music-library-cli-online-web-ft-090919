cc10cb9cf9196fc90673e3aae0db142591d51ebb
Alyssa Yates <bosslys@icloud.com>
2019-10-02 16:52:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,9 @@ class MusicLibraryController
     input = gets.strip
 
     if artist = Artist.find_by_name(input)
-      binding.pry
+      artist.songs.sort_by(&:name).each.with_index(1) do |song, idx|
+        puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
     end
   end
 end
