4a5df7599632c8d582352700683d8fd224d44ea6
Alyssa Yates <bosslys@icloud.com>
2019-10-02 13:35:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,62 @@
+class Song
+# extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    if artist != nil
+      self.artist = artist
+    end
+    if genre != nil
+      self.genre = genre
+    end
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs.push self unless genre.songs.include? self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+end
